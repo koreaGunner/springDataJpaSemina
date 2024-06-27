78a1468ce3f19036fc5833601867ae924cced709
gunners <chakagae123@naver.com>
2024-06-28 00:18:32 +0900

    * 세미나용 코드
      > 간단한 테스트 코드

---
+++ src/main/java/com/semina/springDataJpa/entity/Member.java
@@ -0,0 +1,56 @@
+package com.semina.springDataJpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+) //@NamedQuery의 장점 : 정적쿼리기 때문에 애플리케이션 로딩 시 파싱을 하는데, 오류가 있으면 컴파일 오류를 낸다
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    //@NoArgsConstructor(access = AccessLevel.PROTECTED)로 대체 가능
+//    protected Member() {
+//    }
+
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/main/java/com/semina/springDataJpa/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.semina.springDataJpa.repository;
+
+import com.semina.springDataJpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findTop3HelloBy();
+
+    //    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+}
